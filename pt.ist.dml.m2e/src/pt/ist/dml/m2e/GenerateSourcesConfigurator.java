3e796a8a15933210a80bdef4ea7f782ebbde0230
David Martinho <davidmartinho@gmail.com>
2012-12-12 12:13:50 +0000

    Pushed initial versions of the Eclipse Plugins for both the dml and bennu maven plugins

---
+++ pt.ist.dml.m2e/src/pt/ist/dml/m2e/GenerateSourcesConfigurator.java
@@ -0,0 +1,17 @@
+package pt.ist.dml.m2e;
+
+import org.apache.maven.plugin.MojoExecution;
+import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
+import org.eclipse.m2e.jdt.AbstractJavaProjectConfigurator;
+
+public class GenerateSourcesConfigurator extends AbstractJavaProjectConfigurator {
+	
+	@Override
+	public AbstractBuildParticipant getBuildParticipant(
+			IMavenProjectFacade projectFacade, MojoExecution execution,
+			IPluginExecutionMetadata executionMetadata) {
+		return new GenerateSourcesParticipant(execution);
+	}
+}
